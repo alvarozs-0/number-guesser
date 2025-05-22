78d92f860f03b70e9f987ed963c5d07a4bc8c506
Alvaro Zayas-Bazan Santos <155773873+alvarozs-0@users.noreply.github.com>
2025-05-22 16:00:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ let currentRoundNumber = 1;
 const generateTarget = () => Math.floor(Math.random()* 10);
 
 const compareGuesses = (humanGuess,computerGuess,targetNum) =>{
-    if(humanGuess < 0 || humanGuess > 9){
+    if (humanGuess < 0 || humanGuess > 9){
         alert("Your guess is out of range");
     }
     const computerDiff = Math.abs(computerGuess-targetNum);
     const humanDiff = Math.abs(humanGuess-targetNum);
 
-    if(computerDiff === humanDiff || (humanDiff < computerDiff) ){
+    if (humanDiff <= computerDiff){
         return true;
     }
     else{
